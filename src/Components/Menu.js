1d1c7bfcaad8665bd4d658ff2e0d80b519e6bf5f
Anna Kuznetsova <sutcliff0684@gmail.com>
2021-03-29 20:23:25 +0300

    create modal window

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import dbMenu from './DBMenu';
 import { ListItem } from './listItem';
+import { Banner } from './Banner';
 
 const MenuStyled = styled.main`
     background-color: #f0e4e4;
@@ -12,16 +13,20 @@ const SectionMenu = styled.section`
     padding: 30px;
 `;
 
-export const Menu = () => (
+
+export const Menu = ({setOpenItem}) => (
     <MenuStyled>
+        <Banner/>
         <SectionMenu>
             <h2>Burgers</h2>
-            <ListItem itemList={dbMenu.burger}/>
+            <ListItem itemList={dbMenu.burger}
+                        setOpenItem={setOpenItem}/>
         </SectionMenu>
 
         <SectionMenu>
             <h2>Snack / Beverages</h2>
-            <ListItem itemList={dbMenu.other}/>
+            <ListItem itemList={dbMenu.other}
+                        setOpenItem={setOpenItem}/>
         </SectionMenu>
     </MenuStyled>
 );
